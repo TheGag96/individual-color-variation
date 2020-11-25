ce54780287bfab9f07b10635b9e0ca72ad676975
TheGag96 <thegag96@gmail.com>
2020-11-24 19:48:35 -0600

    Rename Hijack.s to Hijack_HueShift.s

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ patched_arm9bin=arm9_patched.bin
 
 # compile asm and C files
 eval $DEVKITARM/bin/arm-none-eabi-gcc -Wall -Os -march=armv5te -mtune=arm946e-s -fomit-frame-pointer -ffast-math -mthumb -mthumb-interwork -I/opt/devkitpro/libnds/include -DARM9 -c hueshift.c -o hueshift.o
-eval $DEVKITARM/bin/arm-none-eabi-as -march=armv5te -mthumb -mthumb-interwork -c Hijack.s -o Hijack.o
+eval $DEVKITARM/bin/arm-none-eabi-as -march=armv5te -mthumb -mthumb-interwork -c Hijack_HueShift.s -o Hijack_HueShift.o
 eval $DEVKITARM/bin/arm-none-eabi-as -march=armv5te -mthumb -mthumb-interwork -c Hijack_PersonalitySave.s -o Hijack_PersonalitySave.o
 eval $DEVKITARM/bin/arm-none-eabi-as -march=armv5te -mthumb -mthumb-interwork -c Hijack_PersonalityClearPokedex.s -o Hijack_PersonalityClearPokedex.o
 
@@ -18,7 +18,7 @@ dmd binpatch.d
 cp $original_arm9bin $patched_arm9bin
 
 # extract compiled machine code and patch them to specific locations
-eval $DEVKITARM/bin/arm-none-eabi-objcopy -O binary -j .text Hijack.o temp_bin
+eval $DEVKITARM/bin/arm-none-eabi-objcopy -O binary -j .text Hijack_HueShift.o temp_bin
 od -An -t x1 temp_bin | ./binpatch $patched_arm9bin 5023C
 eval $DEVKITARM/bin/arm-none-eabi-objcopy -O binary -j .text hueshift.o temp_bin
 od -An -t x1 temp_bin | ./binpatch $patched_arm9bin 500BC
@@ -31,7 +31,7 @@ od -An -t x1 temp_bin | ./binpatch $patched_arm9bin 50280
 dmd tableprinter.d
 ./tableprinter | ./binpatch $patched_arm9bin 5003C
 
-# hijack palette load function to jump to Hijack.s
+# hijack palette load function to jump to Hijack_HueShift.s
 echo 47 F0 10 F9 | ./binpatch $patched_arm9bin 9018
 
 # hijack GetPkmnData to jump to Hijack_PersonalitySave.s
