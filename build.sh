3dddc00799e73a45eb5f7edce7653e7c5ab04d72
TheGag96 <thegag96@gmail.com>
2021-01-31 01:05:56 -0600

    Instead of hardcoding hijack branches, made utility to generate them

--- build.sh
+++ build.sh
@@ -77,39 +77,41 @@ patch_code Hijack_MiscSprite              504E0
 patch_code Hijack_HallOfFame              50500
 patch_code Hijack_PaletteUpload           50520
 
-# geneate sin/cos table and patch to its location
 dmd tableprinter.d
+dmd makebl.d
+
+# geneate sin/cos table and patch to its location
 ./tableprinter | ./binpatch $patched_arm9bin 5003C
 
 # hijack palette load function to jump to Hijack_HueShift.s
-echo 47 F0 10 F9 | ./binpatch $patched_arm9bin 9018
+./makebl 9018 5023C | ./binpatch $patched_arm9bin 9018
 
 # hijack GetPkmnData to jump to Hijack_PersonalitySave.s
-echo DB F7 C7 FE | ./binpatch $patched_arm9bin 74472 # GetPkmnData
-echo DB F7 47 FE | ./binpatch $patched_arm9bin 74572 # GetBoxPkmnData
+./makebl 74472 50204 | ./binpatch $patched_arm9bin 74472
+./makebl 74572 50204 | ./binpatch $patched_arm9bin 74572
 
 # hijack some pokedex routine to jump to Hijack_PersonalityClearPokedex.s
-echo 28 F0 87 FE | ./binpatch $patched_arm9bin 2756E
+./makebl 2756E 50280 | ./binpatch $patched_arm9bin 2756E
 
 # hijack stuff related to in-battle GBA-styled sprites
-echo 2C F6 B7 FD | ./binpatch $patched_overlay12bin 3B0E # Hijack_BattleSprite.s (on change sprite during switchout)
-echo 2E F6 FB FD | ./binpatch $patched_overlay12bin 1B06 # Hijack_BattleSprite2.s (on change sprite during move animation)
-echo 2C F6 B4 FE | ./binpatch $patched_overlay12bin 39F4 # Hijack_PersonalityTableBuild.s (on change sprite during switchout)
-echo 4D F0 54 F9 | ./binpatch $patched_arm9bin      3034 # Hijack_GbaPal.s (on any GBA-styled sprite palette load)
-echo 12 F6 27 FA | ./binpatch $patched_overlay16bin 2E6E # Hijack_BattleDataPtrSave.s (GetMainBattleData_GetAdrOfPkmnInParty)
+./makebl 0222372E 020502A0 | ./binpatch $patched_overlay12bin 3B0E # Hijack_BattleSprite.s (on change sprite during switchout)
+./makebl 02221726 02050320 | ./binpatch $patched_overlay12bin 1B06 # Hijack_BattleSprite2.s (on change sprite during move animation)
+./makebl 02223614 02050380 | ./binpatch $patched_overlay12bin 39F4 # Hijack_PersonalityTableBuild.s (on change sprite during switchout)
+./makebl 02003034 020502E0 | ./binpatch $patched_arm9bin      3034 # Hijack_GbaPal.s (on any GBA-styled sprite palette load)
+./makebl 0223DFAE 02050400 | ./binpatch $patched_overlay16bin 2E6E # Hijack_BattleDataPtrSave.s (GetMainBattleData_GetAdrOfPkmnInParty)
 
 # hijack stuff needed to get in-battle normal sprites working
-echo 15 F6 4D F9 | ./binpatch $patched_overlay16bin 42   # Hijack_BattleStart.s
-echo 15 F6 D4 F8 | ./binpatch $patched_overlay16bin 154  # Hijack_BattleEnd.s
-echo 06 F6 9E FA | ./binpatch $patched_overlay16bin EDE0 # Hijack_BattleEndCaught.s
-echo 12 F6 2A FE | ./binpatch $patched_overlay16bin 26E8 # Hijack_PersonalityTableBuild2.s
+./makebl 0223B182 02050420 | ./binpatch $patched_overlay16bin 42   # Hijack_BattleStart.s
+./makebl 0223B294 02050440 | ./binpatch $patched_overlay16bin 154  # Hijack_BattleEnd.s
+./makebl 02249F20 02050460 | ./binpatch $patched_overlay16bin EDE0 # Hijack_BattleEndCaught.s
+./makebl 0223D828 02050480 | ./binpatch $patched_overlay16bin 26E8 # Hijack_PersonalityTableBuild2.s
 
 # hijack stuff needed to get misc sprite loads working (HM use, introduction)
-echo 3C F0 26 FF | ./binpatch $patched_arm9bin 13690 # Hijack_MiscSprite.s
+./makebl 02013690 020504E0 | ./binpatch $patched_arm9bin 13690 # Hijack_MiscSprite.s
 
 # hijack stuff needed to get hall of fame sprites working
-echo 14 F6 52 FC | ./binpatch $patched_overlay86bin B18  # Hijack_HallOfFame.s (viewing in actual HoF)
-echo 7E F6 6D FD | ./binpatch $patched_overlay87bin CA2  # Hijack_HallOfFame.s (viewing in PC)
-echo 49 F0 A1 F8 | ./binpatch $patched_arm9bin      73DA # Hijack_PaletteUpload.s
+./makebl 0223BC58 02050500 | ./binpatch $patched_overlay86bin B18  # Hijack_HallOfFame.s (viewing in actual HoF)
+./makebl 021D1A22 02050500 | ./binpatch $patched_overlay87bin CA2  # Hijack_HallOfFame.s (viewing in PC)
+./makebl 020073DA 02050520 | ./binpatch $patched_arm9bin      73DA # Hijack_PaletteUpload.s
 
 rm temp_bin
