03cc4bc96ade0c8b59301e2f4aec1812f540432b
TheGag96 <thegag96@gmail.com>
2024-11-04 22:08:07 -0600

    Hijack_HueShift.s: Fix longstanding battle bug by massively simplifying
    Because `Hijack_PersonalityStore` makes sure the `palette` field in
    `PokepicTemplate` is always filled, we can just grab it by indexing into
    `pokepicManager->pics`. This fixes the first party Pokemon from having
    the wrong color after being switched out and back in (and who knows what
    else). The decomp is a lifesaver!!
    
    I may be able to delete some code from doing this - need to check later.

--- Hijack_HueShift.s
+++ Hijack_HueShift.s
@@ -2,38 +2,27 @@
 
 Hijack: @ hook at 0x999C
   ldr r0, [r0, #0x0C] @ pointer to palette
-  push {r0, r2, lr}
+  push {lr}
+  push {r0-r3}
 
-  ldr r1, =0x023C81A4 @ location of "free ram" area
-  ldr r2, [r1, #0x18]
-  ldr r3, =0x00BA771E
-  cmp r2, r3
-  beq .battle
+  ldr r2, [sp, #0x28]   @ contains current sprite index, i (0-3)
+  mov r1, #0xAC         @ size of Pokepic struct
 
-  .typical:
-
-  ldr r1, [r1, #0x20]   @ contains personality value of last read pkmn data
-  b .shift
-
-  .battle:
-
-  ldr r2, [sp, #0x20]   @ contains current battle sprite index (0-3)
-  lsl r2, r2, #2
-  ldr r1, [r1, r2]      @ index into previously-built active pokemon personality table
-
-  .shift:
+  @ r5 contains argument to PokepicManager_BufferPlttData, pokepicManager
+  @ so, load pokepicManager->pics[i].template.personality
+  @ luckily, Hijack_PersonalityStore makes sure this always filled!
+  mul r1, r2
+  add r1, r5, r1
+  ldr r1, [r1, #0x10]
 
   mov r2, #0  @ r1 contians the whole personality value, not just a hue shift table index.
 
   ldr r3, =0x023C8081 @ location of code from hueshift.c
   blx r3
 
-  pop {r1, r2, r3}    @ note that the saved r0 was pushed into r1, which effectively does what the hijacked code did
-  ldr r0, [sp, #0x8]
+  @ restore old code
+  pop {r0-r3}
+  mov r1, r0
+  ldr r0, [sp, #0xC]
 
-  bx r3
-
-@ 00 49 08 47 05 24 10 02
-
-
-@ C0 68 04 21 05 B5 02 4B 98 47 0E BC 02 98 18 47 BC 00 05 02 41 13
+  pop {pc}
