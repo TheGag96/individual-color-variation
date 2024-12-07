33544ec322d27a3a5e4716329115e89e3947fda4
TheGag96 <thegag96@gmail.com>
2024-12-06 23:42:26 -0600

    build.sh: Update C compilation for compatibility with changes in libnds 2.0.0
    ...and fix some disgusting hardcoded paths while I'm at it.

--- build.sh
+++ build.sh
@@ -22,7 +22,9 @@ patched_overlay119bin=overlay119_patched.bin
 
 # compile asm and C files
 function compile_c {
-  eval $DEVKITARM/bin/arm-none-eabi-gcc -Wall -Os -march=armv5te -mtune=arm946e-s -fomit-frame-pointer -ffast-math -mthumb -mthumb-interwork -I/opt/devkitpro/libnds/include -DARM9 -c $1 -o $(basename $1 .c).o
+  eval $DEVKITARM/bin/arm-none-eabi-gcc -Wall -Os -march=armv5te -mtune=arm946e-s -fomit-frame-pointer -ffast-math \
+    -mthumb -mthumb-interwork "-I$DEVKITPRO/libnds/include" "-I$DEVKITPRO/calico/include" -D__NDS__ -DARM9 \
+    -c $1 -o $(basename $1 .c).o
 }
 
 function compile_asm {
